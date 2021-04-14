8c6cd1866ddd34ba8e6e978e42f023ea8b5c2b3e
baikai-li <baikai.study@gmail.com>
2021-04-14 16:51:06 +0800

    fix bug and transfer test_ins.py and some api from mmdetv1.0

--- mmdet/core/evaluation/__init__.py
+++ mmdet/core/evaluation/__init__.py
@@ -1,6 +1,7 @@
 from .class_names import (cityscapes_classes, coco_classes, dataset_aliases,
                           get_classes, imagenet_det_classes,
                           imagenet_vid_classes, voc_classes)
+from .coco_utils import coco_eval, fast_eval_recall, results2json, results2json_segm
 from .eval_hooks import DistEvalHook, EvalHook
 from .mean_ap import average_precision, eval_map, print_map_summary
 from .recall import (eval_recalls, plot_iou_recall, plot_num_recall,
@@ -11,5 +12,6 @@ __all__ = [
     'coco_classes', 'cityscapes_classes', 'dataset_aliases', 'get_classes',
     'DistEvalHook', 'EvalHook', 'average_precision', 'eval_map',
     'print_map_summary', 'eval_recalls', 'print_recall_summary',
-    'plot_num_recall', 'plot_iou_recall'
+    'plot_num_recall', 'plot_iou_recall', 'coco_eval', 'fast_eval_recall',
+    'results2json', 'results2json_segm'
 ]
